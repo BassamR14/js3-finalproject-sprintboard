29afd76f8e56bfaf153693a4910746ec49b60ec3
BassamR14 <bassam.ruwaida@gmail.com>
2026-09-21 16:19:15 +0200

    removed console.log

--- src/app/components/KanbanBoard.tsx
+++ src/app/components/KanbanBoard.tsx
@@ -5,7 +5,6 @@ import KanbanColumn from "./KanbanColumn";
 import styles from "./KanbanBoard.module.css";
 
 export default function KanbanBoard({ data }: { data: Issue[] }) {
-  console.log(data[0]);
   const columns = groupIssuesByColumn(data);
 
   return (
